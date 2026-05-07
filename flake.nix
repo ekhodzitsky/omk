75e5a31620d49847a46e48f02e51f6ec5af891f6
ekhodzitsky <e@khodzitsky.ru>
2026-05-07 20:30:15 +0300

    chore: add Nix flake + rust-toolchain.toml
    - flake.nix with devShell and package build
    - rust-toolchain.toml pinning stable + rustfmt + clippy
    - .envrc for direnv users

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Multi-agent orchestration for Kimi CLI";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "omk";
+          version = "0.1.1";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = [ pkgs.openssl ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.darwin.apple_sdk.frameworks.Security
+            pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+          ];
+          meta = with pkgs.lib; {
+            description = "Multi-agent orchestration for Kimi CLI";
+            homepage = "https://github.com/ekhodzitsky/oh-my-kimi";
+            license = licenses.mit;
+            maintainers = [ ];
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustToolchain
+            cargo-watch
+            cargo-deny
+            tmux
+            kimi
+          ];
+        };
+      });
+}
